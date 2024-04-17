1a4bedde6a79a2c5f054bf591e87ce2a5d701015
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2024-04-17 15:58:01 -0700

    maps

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,14 +1,45 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+const (
+	ErrorNotFound = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrorNotFound
 	}
 	return definition, nil
 }
 
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	}
+}
