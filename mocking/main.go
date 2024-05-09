0b1d7625b3c862ce3c0f922de308522d2789b4bf
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2024-05-09 15:57:25 -0700

    latest

--- mocking/main.go
+++ mocking/main.go
@@ -30,6 +30,16 @@ type SpyCountdownOperations struct {
 	Calls []string
 }
 
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+const write = "write"
+const sleep = "sleep"
 
 const finalWord = "Go!"
 const countdownStart = 3
